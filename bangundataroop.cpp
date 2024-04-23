44794edfbc072be970aa8df259d7f6420078258e
Asti22 <uun3461@gmail.com>
2024-04-23 15:51:41 +0700

    membuat member display

--- bangundataroop.cpp
+++ bangundataroop.cpp
@@ -18,6 +18,12 @@ class bangunDatar{
        float hitungLuas(){
         return panjang*Lebar;
        }
+
+       void display(){
+           cout <<"Panjangnya =" <<panjang<<endl;
+           cout <<"Lebarnya ="<<Lebar<<endl;
+           cout <<"Luasnya =" <<hitungLuas<<endl;
+       }
     
 
 
